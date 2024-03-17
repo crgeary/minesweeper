faed844b3d858a615a1a3271f8a3e9df66f9eb46
Christopher Geary <hello@crgeary.com>
2024-03-17 22:11:30 +0000

    Set cell colours

--- app/src/components/minefield/cell.component.tsx
+++ app/src/components/minefield/cell.component.tsx
@@ -14,11 +14,19 @@ export function Cell({ isFlagged, isRevealed, state, className, ...props }: Cell
     <button
       className={classNames(
         className,
-        "w-8 h-8 flex items-center justify-center flex-col border-l border-b border-black text-sm font-bold",
+        "w-8 h-8 flex items-center justify-center flex-col border-l border-b border-black text-lg font-bold",
         {
           "bg-gray-100": isFlagged,
-          "bg-white": isRevealed,
           "bg-yellow-100": isRevealed && state === 0,
+          "bg-yellow-50": isRevealed && state > 0,
+          "text-blue-800": isRevealed && state === 1,
+          "text-green-700": isRevealed && state === 2,
+          "text-red-600": isRevealed && state === 3,
+          "text-indigo-800": isRevealed && state === 4,
+          "text-red-900": isRevealed && state === 5,
+          "text-teal-600": isRevealed && state === 6,
+          "text-black": isRevealed && state === 7,
+          "text-gray-500": isRevealed && state === 8,
         },
       )}
       {...props}
